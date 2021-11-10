dcf3b64bd63c9232697694ffc6534492282383ea
rain0100 <68754474+rain0100@users.noreply.github.com>
2021-11-10 17:48:45 -0500

    start queen

--- chessgame/queen.py
+++ chessgame/queen.py
@@ -1,1 +1,41 @@
-print("hello world")
+import pygame
+
+
+class Queen:
+
+    def __init__(self, board_grid, is_white, file, rank):
+        self.board_grid = board_grid
+        self.is_white = is_white
+        self.img = pygame.image.load('Images/WhiteKing.png').convert()
+        self.file = file
+        self.rank = rank
+        self.has_moved = False
+
+    def get_img(self):
+        return self.img
+
+    def get_has_moved(self):
+        return self.has_moved
+
+    def get_file(self):
+        return self.file
+
+    def get_rank(self):
+        return self.rank
+
+    def set_file(self, file):
+        self.file = file
+
+    def set_rank(self, rank):
+        self.rank = rank
+
+    # get_legal_moves(String prev_move)
+    # e.g. if the previous move was Bishop to h4, board_grid["a"][3].get_legal_moves("Bh4")
+    # return list of legal squares to move to (e.g. ["a1", "a2", "a3", etc.])"""
+    def get_legal_moves(self, prev_move):
+        pass
+
+    # move(String destination) modifies the state of the board based on the location the piece is moving to (and
+    # takes care of any captures that may have happened) e.g. board_grid["a"][3].move("b2")
+    def move(self, destination):
+        pass
