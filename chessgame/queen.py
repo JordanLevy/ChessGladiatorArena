a52cb9d88f81e194a0cc6185615e91bde8e2ffbd
rain0100 <68754474+rain0100@users.noreply.github.com>
2021-11-15 21:50:39 -0500

    Update queen.py

--- chessgame/queen.py
+++ chessgame/queen.py
@@ -47,14 +47,67 @@ class Queen:
     def get_legal_captures(self):
         return []
 
+
+
+    def defended_by_enemy(self, f, r):
+        files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        target = f + str(r)
+        for i in range(0, 7):
+            for j in range(1, 8):
+                s = self.board_grid[files[i]][j]
+                if s and (not type(s) is EnPassant) and (self.is_white != s.get_is_white()) and (target in s.get_legal_captures()):
+                    return True
+        return False
+
+
     # get_legal_moves(String prev_move)
     # e.g. if the previous move was Bishop to h4, board_grid["a"][3].get_legal_moves("Bh4")
     # return list of legal squares to move to (e.g. ["a1", "a2", "a3", etc.])"""
     def get_legal_moves(self):
+        files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         legal_moves = []
+        w = self.get_is_white()
+        b = self.get_is_black()
+        file_num = files.index(self.file)
+
+        for i in range(-7, 8):
+            f = file_num + i
+            # if file out of bounds
+            if f < 0 or f > 7:
+                continue
+            for j in range(-7, 8):
+                r = self.rank + j
+                # if rank out of bounds
+                if r < 1 or r > 8:
+                    continue
+                if f != file_num and i != j and i != -j and r != self.rank:
+                    continue
+                s = self.board_grid[files[f]][r]
+                # if square is occupied by a friendly piece, it's not en en passant marker, and it's friendly
+                if s and not type(s) is EnPassant and w == s.get_is_white():
+                    # king is blocked by its own piece
+                    continue
+                # if square is controlled by an enemy piece
+                if self.defended_by_enemy(files[f], r):
+                    continue
+                legal_moves.append(files[f] + str(r))
+
         return legal_moves
+
 
     # move(String destination) modifies the state of the board based on the location the piece is moving to (and
     # takes care of any captures that may have happened) e.g. board_grid["a"][3].move("b2")
     def move(self, f, r):
-        return False
+        legal_moves = self.get_legal_moves()
+        if not f + str(r) in legal_moves:
+            print('illegal move')
+            return False
+        is_capture = not self.board_grid[f][r] is None
+        is_en_passant = False
+        self.move_list.append(Move(self.is_white, 'K', self.file, self.rank, is_capture, is_en_passant, f, r))
+        self.board_grid[self.file][self.rank] = None
+        self.file = f
+        self.rank = r
+        self.board_grid[self.file][self.rank] = self
+        self.has_moved = True
+        return True
